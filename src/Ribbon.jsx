0e404843bfe60ec63b3d76354301469240cb5792
George Michael <161012911+georgem7154@users.noreply.github.com>
2025-03-05 17:02:33 +0530

    ribbon added txt button

--- src/Ribbon.jsx
+++ src/Ribbon.jsx
@@ -1,9 +1,16 @@
-
-
 const Ribbon = () => {
   return (
-    <div className="h-36 top-0 w-full fixed bg-red-400">Ribbon</div>
-  )
-}
+    <div className="h-36 top-0 w-full fixed bg-red-400">
+      <div className="container mx-auto flex justify-between items-center h-full">
+        <div className="font-montserrat flex font-bold text-4xl">NAME IT!</div>
+        <div className="font-montserrat flex font-semibold text-2xl justify-between mr-36 w-7/12 flex-row"> 
+          <button>Book</button>
+          <button>Plan</button>
+          <button>Travel</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Ribbon
+export default Ribbon;
