de9240051ff130098ea7b64d7fb55400db80a9b2
George Michael <161012911+georgem7154@users.noreply.github.com>
2025-03-15 11:46:09 +0530

    final

--- server/index.js
+++ server/index.js
@@ -1,40 +1,52 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
-require('dotenv').config(); 
+require("dotenv").config(); // Load variables from .env
+
+// Access the variable
 const dBURI = process.env.DB_URI;
 
+console.log(dBURI); // Check that it works
 
-
-mongoose.connect(dBURI)
+mongoose
+  .connect(dBURI)
   .then((result) => app.listen(5000))
-  .catch((err) => console.error('MongoDB connection error:', err));
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 // Define the model for the existing collection without a schema
-const String = mongoose.model('String', new mongoose.Schema({}, { strict: false }), 'string');
+const String = mongoose.model(
+  "String",
+  new mongoose.Schema({}, { strict: false }),
+  "string"
+);
 
-app.get('/string', (req, res) => {
+app.get("/string", (req, res) => {
   const newString = new String({
-    text: 'This is a blog post'
+    text: "This is a blog post",
   });
-  newString.save()
+  newString
+    .save()
     .then((result) => {
       res.send(result);
     })
-    .catch((err) => { console.log(err); });
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
-app.get('/allblogs', (req, res) => {
+app.get("/allblogs", (req, res) => {
   String.find()
     .then((result) => {
       res.send(result);
     })
-    .catch((err) => { console.log(err); });
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
-app.use('/', (req, res) => {
-  res.send('Server is up and running');
+app.use("/", (req, res) => {
+  res.send("Server is up and running");
 });
